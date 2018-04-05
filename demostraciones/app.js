1ede0fa41edb1207f34e4f50ecdfcbf285f67f2a
test <test@test.com>
2018-04-05 16:53:52 -0500

    Inicio de repositorio

---
+++ demostraciones/app.js
@@ -0,0 +1,23 @@
+/*
+
+function saludar(nombre){
+  console.log("Hola"+ nombre)
+}
+
+var wolverine = {
+  nombre:"Logan"
+};
+
+saludar(wolverine)
+
+*/
+
+function saludar(nombre:string){
+  console.log("Hola "+ nombre)
+}
+
+var wolverine = {
+  nombre:"Logan"
+};
+
+saludar(wolverine.nombre)
